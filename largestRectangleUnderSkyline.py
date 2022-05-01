dd7d20a66d2e90776c8063a07bea3c3cd808187f
Niramay Thaker <70151582+NiramayThaker@users.noreply.github.com>
2022-05-01 18:26:41 +0530

    Question: algoexpert difficulty: HARD
        data = [1, 3, 3, 2, 4, 1, 5, 3, 2]
        data = [4, 4, 4, 2, 2, 1]
        data = [5, 5, 2, 2, 4, 1]
        data = [1, 3, 3, 2, 4, 1, 5, 3]
    Test Cases: 
    
        data = [1, 2, 3, 4, 5, 11]
        data = [25, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 ,1, 1, 1, 1]
        data = [20, 2, 2, 2, 2, 2, 10 , 5, 5, 5, 4 , 4]
        data = [5, 10, 5, 15, 10, 25]
        data = [1, 1, 1, 1]
        data = []
        data = [10]

---
+++ largestRectangleUnderSkyline.py
@@ -0,0 +1,35 @@
+def largestRectangleUnderSkyline(buildings):
+
+    ans_arr = []
+
+    for i in range(len(buildings)):
+
+        checkFor = buildings[i]
+        endAt = len(buildings)
+        ans = 0
+
+        for rightEle in range(i, endAt):
+            if buildings[rightEle] >= checkFor:
+                ans += 1          
+            else:
+                break
+
+        for leftEle in range(i, i-1, -1):
+            if i == 0:
+                break
+            elif buildings[leftEle] >= checkFor:
+                if buildings[leftEle - 1] >= checkFor:
+                    ans += 1
+                else:
+                    break
+
+        ans_arr.append(checkFor * ans)
+
+    return max(ans_arr)
+
+
+if __name__ == '__main__':
+
+    data = [20, 2, 2, 2, 2, 2, 10, 5, 5, 5, 4, 4]
+    output = largestRectangleUnderSkyline(data)
+    print(output)
